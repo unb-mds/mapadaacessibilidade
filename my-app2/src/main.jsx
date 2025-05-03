e94340df4d2d3c14aa942089f51ecd2b03a8b363
Dexmachi <232001371@aluno.unb.br>
2025-05-03 16:14:35 -0300

    ponto de controle frontend app.jsx funcional protótipo

--- my-app2/src/main.jsx
+++ my-app2/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx ou index.js
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css' // Importe seu arquivo CSS principal aqui
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
