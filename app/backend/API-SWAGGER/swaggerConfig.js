eef4ce0faea57710972fa80f670ca6b8720e7f15
Maria Laura Regis <marialauraregi@gmail.com>
2025-06-10 19:47:22 -0300

    Adicionado o arquivo swagger config para documentação com swagger

---
+++ app/backend/API-SWAGGER/swaggerConfig.js
@@ -0,0 +1,17 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const YAML = require('yamljs');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API - Mapa da Acessibilidade',
+      version: '1.0.0',
+      description: 'Documentação das rotas da API para gerenciamento de locais acessíveis',
+
+  apis: ['./src/routes/*.js', './src/controllers/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
