c122494628b139f99ee33ee6e38835a89f720e55
Maria Laura Regis <marialauraregi@gmail.com>
2025-06-10 19:55:56 -0300

    Adicionado configuração de swagger em server.js

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -5,6 +5,15 @@ import usuariosRoutes from "./routes/usuariosRoutes.js";
 import acessibilidadeRouter from "./routes/acessibilidadeRouter.js";
 import acessibilidadeLocalRouter from "./routes/acessibilidadeLocalRouter.js";
 
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../docs/swaggerConfig'); 
+
+const locaisRoutes = require('./routes/locaisRouter');
+const usuariosRoutes = require('./routes/usuariosRoutes');
+const acessibilidadeRouter = require('./routes/acessibilidadeRouter');
+
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
@@ -21,6 +30,10 @@ app.get("/usuarios", async (req, res) => {
   res.status(200).json(users);
 });
 
+// Rota da documentação Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.listen(port, () => {
   console.log(`Servidor funcionando http://localhost:${port}`);
+  console.log(`Documentação disponível em http://localhost:${port}/api-docs`);
 });
