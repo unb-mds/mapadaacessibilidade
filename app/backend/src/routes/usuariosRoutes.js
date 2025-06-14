4ea6eca748f1199d21d8b4b1af5ad3640a3ed803
Caio de Andrade <84874942+CaioSoandrd@users.noreply.github.com>
2025-06-14 00:06:40 -0300

    Hotfix: Feat listar usuários

--- app/backend/src/routes/usuariosRoutes.js
+++ app/backend/src/routes/usuariosRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   cadastrarUsuario,
   loginUsuario,
+  listarUsuarios,
 } from "../controllers/usuariosController.js";
 
 const router = express.Router();
 
+router.get("/usuarios", listarUsuarios);
 router.post("/usuarios", cadastrarUsuario);
 router.post("/usuarios/login", loginUsuario);
 
