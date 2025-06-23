67e2a6788b19e5311559e5fefac3731f90546567
Caio Rocha <232001371@aluno.unb.br>
2025-06-23 14:50:37 -0300

    Nova branch para testes

---
+++ app/backend/src/controllers/createAcessibilidade.test.js
@@ -0,0 +1,5 @@
+import createAcessibilidade from "./acessibilidadeController.js";
+
+describe("createAcessibilidade", () => {
+  it("Cria uma nova entra de acessebilidade com sucesso", async () => {});
+});
